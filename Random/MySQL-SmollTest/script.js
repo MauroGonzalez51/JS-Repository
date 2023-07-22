4389bf95e6a6ee7135b8352077f585a6bdc2da81
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-07-22 15:37:52 -0500

    A experiment with the idea of sending data to a localhost database turned into a fecth practice with reqres.in

--- Random/MySQL-SmollTest/script.js
+++ Random/MySQL-SmollTest/script.js
@@ -1,5 +1,3 @@
-import sendDataToDatabase from "./database.js";
-
 const form = document.querySelector(".form");
 
 form.addEventListener("submit", (event) => {
@@ -9,5 +7,31 @@ form.addEventListener("submit", (event) => {
     const { value: age } = document.getElementById("age");
     const { value: email } = document.getElementById("email");
 
-    sendDataToDatabase({ name, age, email })
+    sendData({ name, age, email })
+        .then((response) => {
+            console.log(response);
+            clearInputs(form);
+        })
+        .catch((error) => console.error(error));
 });
+
+const sendData = async ({ name, age, email }) => {
+    return new Promise((resolve, reject) => {
+        fetch("https://reqres.in/api/users", {
+            method: "POST",
+            body: JSON.stringify({ name, age, email }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((response) => resolve(response))
+            .catch((error) => reject(error));
+    });
+};
+
+const clearInputs = (parentElement) => {
+    parentElement.querySelectorAll(".input").forEach((input) => {
+        input.value = "";
+    });
+};
