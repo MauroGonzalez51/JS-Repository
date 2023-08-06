6be5092a8002687515e6de5952838e5f332d5518
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2023-08-05 19:34:11 -0500

    GetFormValues + Index (Sketch) Done

---
+++ Random/Schedule/1.2/formValues.js
@@ -0,0 +1,16 @@
+const getFormValues = (form) => {
+    const inputs = form.querySelectorAll(".form-field__input");
+    const selects = form.querySelectorAll(".form-field > .select");
+
+    const inputValues = Object.fromEntries(
+        Array.from(inputs).map((input) => [input.id, input.value])
+    );
+
+    const selectValues = Object.fromEntries(
+        Array.from(selects).map((select) => [select.id, select.value])
+    );
+
+    return { ...inputValues, ...selectValues };
+};
+
+export { getFormValues }
